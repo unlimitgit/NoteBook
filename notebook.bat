15bb4da66a43b3d49308937dbd2643cb77777ca6
Jim Dai <jim.dai@autoliv.com>
2017-10-04 15:06:02 -0400

    Add batch file for automatically running

---
+++ notebook.bat
@@ -0,0 +1,7 @@
+javac ./com/notebook/GlobalVariables.java
+javac ./com/notebook/EditDisplay.java
+javac ./com/notebook/CreateStyles.java
+javac ./com/notebook/MenuEdit.java
+javac ./com/notebook/MessageProcess.java
+javac NoteBook.java
+java NoteBook
