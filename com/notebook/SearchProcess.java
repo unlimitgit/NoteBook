b7dd7b0c2dd364a95faf009c176d5db9a77444fd
Jianhua Dai <jimdai09@gmail.com>
2017-10-06 12:05:32 -0400

    Add string compare case insensitive function

--- com/notebook/SearchProcess.java
+++ com/notebook/SearchProcess.java
@@ -36,7 +36,8 @@ public class SearchProcess
 						final Scanner scanner = new Scanner(file);
 						while (scanner.hasNextLine()) {
 						   final String lineFromFile = scanner.nextLine();
-						   if(lineFromFile.contains(keyWord)) { 
+						   //if(lineFromFile.contains(keyWord)) { 
+						   if(containsIgnoreCase(lineFromFile,keyWord)) { 
 							   // a match!
 							   //System.out.println("I found " + keyWord + " in file " +file.getName().substring(0, file.getName().lastIndexOf('.')));
 							   try {
@@ -57,6 +58,27 @@ public class SearchProcess
 			e.printStackTrace();
 		}
 	}
+	
+	public static boolean containsIgnoreCase(String src, String what) {
+    final int length = what.length();
+    if (length == 0)
+        return true; // Empty string is contained
+
+    final char firstLo = Character.toLowerCase(what.charAt(0));
+    final char firstUp = Character.toUpperCase(what.charAt(0));
+
+    for (int i = src.length() - length; i >= 0; i--) {
+        // Quick check before calling the more expensive regionMatches() method:
+        final char ch = src.charAt(i);
+        if (ch != firstLo && ch != firstUp)
+            continue;
+
+        if (src.regionMatches(true, i, what, 0, length))
+            return true;
+    }
+
+    return false;
+}
 		
 }
 
